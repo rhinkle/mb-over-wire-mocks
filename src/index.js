e41b9e289f3e23f0413b421a37974df770f28d5b
Ryan Hinkle <ryan.hinkle@integral.io>
2021-04-01 08:15:10 -0400

    Working example of MBtests

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const mb = require('mountebank');
 const mbHelper = require('mountebank-helper');
 const config = require('./config');
 
-
-// create the skeleton for the imposter (does not post to MB)
-const firstImposter = new mbHelper.Imposter({ 
-  'mountebankPort': 5000,
-  'imposterPort' : 5001 });
+const firstImposter = new mbHelper.Imposter({
+    'mountebankPort': 5000,
+    'imposterPort' : 5001
+});
  
 const sample_response = {
     'uri' : '/hello',
@@ -17,11 +16,9 @@ const sample_response = {
       'responseBody' : JSON.stringify({ 'hello' : 'world' })
     }
   };
-mbHelper.
 
-  // add our responses to our imposter
 firstImposter.addRoute(sample_response);
- 
+
 mb.create({
   port: config.port,
   pidfile: '../mb.pid',
